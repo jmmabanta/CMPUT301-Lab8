4cd43e72b1aa958c1559825c632bd3d6b01080b8
John Mabanta <johnmabanta51@gmail.com>
2023-03-09 16:23:47 -0700

    Added addCity test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -6,11 +6,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList customList;
+
+    @BeforeEach
+    private void setUp() {
+        customList = new CustomList(null, new ArrayList<>());
+    }
+
+    @Test
+    public void testAddCities() {
+        assertEquals(0, customList.getCount());
+        customList.addCity(new City("Edmonton", "Alberta"));
+        assertEquals(1, customList.getCount());
+        customList.addCity(new City("Vancouver", "British Columnbia"));
+        assertEquals(2, customList.getCount());
+    }
 
 }
